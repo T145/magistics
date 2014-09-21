269f6b4aee5ce80734f2ca4576b4303f39dc482f
T145 <taylorshuler@aol.com>
2014-09-21 19:49:57 -0400

    Actually fixed bash building

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 FORGE_VERSION=1.7.10-10.13.1.1217
 
 curl -L -O -k http://files.minecraftforge.net/maven/net/minecraftforge/forge/$FORGE_VERSION/forge-$FORGE_VERSION-src.zip
-mkdir forge && mkdir eclipse/.metadata
+mkdir forge
 unzip forge-$FORGE_VERSION-src.zip -d forge
-mv forge/eclipse/.metadata eclipse/.metadata
+cp -rf forge/eclipse/.metadata eclipse/.metadata
 rm -rf forge && rm -rf forge-$FORGE_VERSION-src.zip
 gradle setupDecompWorkspace && gradle eclipse
