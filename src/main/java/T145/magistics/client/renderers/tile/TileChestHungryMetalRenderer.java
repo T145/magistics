b5b2301a2696330aa1725f47035907d28e0459b2
Taylor Shuler <taylorshuler@aol.com>
2014-09-28 23:15:50 -0400

    Tidied up the hungry iron chest renderer

--- src/main/java/T145/magistics/client/renderers/tile/TileChestHungryMetalRenderer.java
+++ src/main/java/T145/magistics/client/renderers/tile/TileChestHungryMetalRenderer.java
@@ -20,6 +20,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 import T145.magistics.common.blocks.BlockChestHungryMetal;
@@ -28,56 +29,53 @@ import T145.magistics.common.tiles.TileChestHungryMetal;
 
 import com.google.common.primitives.SignedBytes;
 
-import cpw.mods.ironchest.IronChestType;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import cpw.mods.ironchest.IronChestType;
 
 @SideOnly(Side.CLIENT)
 public class TileChestHungryMetalRenderer extends TileEntitySpecialRenderer {
-	public Random random;
-	public RenderItem itemRenderer;
-	public ModelChest model;
+	public Random random = new Random();
+	public RenderItem itemRenderer = new RenderItem() {
+		@Override
+		public byte getMiniBlockCount(ItemStack stack, byte original) {
+			return SignedBytes.saturatedCast(Math.min(stack.stackSize / 32, 15) + 1);
+		}
+
+		@Override
+		public byte getMiniItemCount(ItemStack stack, byte original) {
+			return SignedBytes.saturatedCast(Math.min(stack.stackSize / 32, 7) + 1);
+		}
+
+		@Override
+		public boolean shouldBob() {
+			return false;
+		}
+
+		@Override
+		public boolean shouldSpreadItems() {
+			return false;
+		}
+	};
+	public ModelChest chestModel = new ModelChest();
 
 	public static float[][] shifts = { { 0.3F, 0.45F, 0.3F }, { 0.7F, 0.45F, 0.3F }, { 0.3F, 0.45F, 0.7F }, { 0.7F, 0.45F, 0.7F }, { 0.3F, 0.1F, 0.3F }, { 0.7F, 0.1F, 0.3F }, { 0.3F, 0.1F, 0.7F }, { 0.7F, 0.1F, 0.7F }, { 0.5F, 0.32F, 0.5F } };
 
 	@Override
-	public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float partialTick) {
-		renderChestAt((TileChestHungryMetal) tileentity, x, y, z, partialTick);
+	public void renderTileEntityAt(TileEntity chestentity, double x, double y, double z, float partialTick) {
+		renderChestAt((TileChestHungryMetal) chestentity, x, y, z, partialTick);
 	}
 
 	public TileChestHungryMetalRenderer() {
-		model = new ModelChest();
-		random = new Random();
-		itemRenderer = new RenderItem() {
-			@Override
-			public byte getMiniBlockCount(ItemStack stack, byte original) {
-				return SignedBytes.saturatedCast(Math.min(stack.stackSize / 32, 15) + 1);
-			}
-
-			@Override
-			public byte getMiniItemCount(ItemStack stack, byte original) {
-				return SignedBytes.saturatedCast(Math.min(stack.stackSize / 32, 7) + 1);
-			}
-
-			@Override
-			public boolean shouldBob() {
-				return false;
-			}
-
-			@Override
-			public boolean shouldSpreadItems() {
-				return false;
-			}
-		};
 		itemRenderer.setRenderManager(RenderManager.instance);
 	}
 
-	public void renderChestAt(TileChestHungryMetal tile, double x, double y, double z, float partialTick) {
-		if (tile == null)
+	public void renderChestAt(TileChestHungryMetal chest, double x, double y, double z, float partialTick) {
+		if (chest == null)
 			return;
-		int facing = tile.getFacing();
+		int facing = chest.getFacing();
 
-		BlockChestHungryMetal.Types type = BlockChestHungryMetal.Types.values()[tile.getBlockMetadata()];
+		BlockChestHungryMetal.Types type = BlockChestHungryMetal.Types.values()[chest.getBlockMetadata()];
 		ResourceLocation loc = TextureHelper.ironChestTextures.get(type);
 		if (loc != null)
 			bindTexture(loc);
@@ -88,43 +86,42 @@ public class TileChestHungryMetalRenderer extends TileEntitySpecialRenderer {
 		glTranslatef((float) x, (float) y + 1.0F, (float) z + 1.0F);
 		glScalef(1.0F, -1F, -1F);
 		glTranslatef(0.5F, 0.5F, 0.5F);
-		int k = 0;
-		if (facing == 2) {
-			k = 180;
+		int mod = 0;
+		switch (facing) {
+		case 2:
+			mod = 180;
+			break;
+		case 4:
+			mod = 90;
+			break;
+		case 5:
+			mod = -90;
+			break;
 		}
-		if (facing == 3) {
-			k = 0;
-		}
-		if (facing == 4) {
-			k = 90;
-		}
-		if (facing == 5) {
-			k = -90;
-		}
-		glRotatef(k, 0.0F, 1.0F, 0.0F);
+		glRotatef(mod, 0.0F, 1.0F, 0.0F);
 		glTranslatef(-0.5F, -0.5F, -0.5F);
-		float lidangle = tile.prevLidAngle + (tile.lidAngle - tile.prevLidAngle) * partialTick;
+		float lidangle = chest.prevLidAngle + (chest.lidAngle - chest.prevLidAngle) * partialTick;
 		lidangle = 1.0F - lidangle;
 		lidangle = 1.0F - lidangle * lidangle * lidangle;
-		model.chestLid.rotateAngleX = -(lidangle * (float) Math.PI / 2.0F);
-		model.renderAll();
+		chestModel.chestLid.rotateAngleX = -(lidangle * (float) Math.PI / 2.0F);
+		chestModel.renderAll();
 		glDisable(GL12.GL_RESCALE_NORMAL);
 		glPopMatrix();
 		glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		if (IronChestType.values()[tile.getBlockMetadata()].isTransparent() && tile.getDistanceFrom(field_147501_a.field_147560_j, field_147501_a.field_147561_k, field_147501_a.field_147558_l) < 128d) {
+		if (IronChestType.values()[chest.getBlockMetadata()].isTransparent() && chest.getDistanceFrom(field_147501_a.field_147560_j, field_147501_a.field_147561_k, field_147501_a.field_147558_l) < 128d) {
 			random.setSeed(254L);
 			float shiftX, shiftY, shiftZ, blockScale = 0.7F;
 			int shift = 0;
-			if (tile.getTopItemStacks()[1] == null) {
+			if (chest.getTopItemStacks()[1] == null) {
 				shift = 8;
 				blockScale = 0.85F;
 			}
 			glPushMatrix();
-			glDisable(2896 /* GL_LIGHTING */);
+			glDisable(GL11.GL_LIGHTING);
 			glTranslatef((float) x, (float) y, (float) z);
 			EntityItem customitem = new EntityItem(field_147501_a.field_147550_f);
 			customitem.hoverStart = 0f;
-			for (ItemStack item : tile.getTopItemStacks()) {
+			for (ItemStack item : chest.getTopItemStacks()) {
 				if (shift > shifts.length)
 					break;
 				if (item == null) {
@@ -143,7 +140,7 @@ public class TileChestHungryMetalRenderer extends TileEntitySpecialRenderer {
 				itemRenderer.doRender(customitem, 0, 0, 0, 0, 0);
 				glPopMatrix();
 			}
-			glEnable(2896 /* GL_LIGHTING */);
+			glEnable(GL11.GL_LIGHTING);
 			glPopMatrix();
 			glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		}
