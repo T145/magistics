bb310674a05c37d874b70d64736a98032ac399d7
T145 <taylorshuler@aol.com>
2014-11-06 09:41:52 -0500

    Removed Thaumcraft stuff temporarily

--- src/main/java/T145/magistics/common/config/MagisticsConfig.java
+++ src/main/java/T145/magistics/common/config/MagisticsConfig.java
@@ -22,7 +22,7 @@ import T145.magistics.common.tiles.TileChestHungryEnder;
 import T145.magistics.common.tiles.TileChestHungryMetal;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-public class MagisticsConfig {
+public class MagisticsConfig extends MagisticsLogger {
 	public static Configuration config;
 
 	public static boolean colored_names, debug, low_gfx;
@@ -36,26 +36,24 @@ public class MagisticsConfig {
 	};
 
 	public static Item items[] = {
-		new ItemResources().setCreativeTab(Magistics.tabMagistics).setHasSubtypes(true).setUnlocalizedName(itemName[0]),
-		new ItemCruelMask(ThaumcraftApi.armorMatThaumium, 2, 0).setCreativeTab(Magistics.tabMagistics).setMaxDamage(100).setMaxStackSize(1).setUnlocalizedName(itemName[1]),
-		new ItemEldritchKeystone().setCreativeTab(Magistics.tabMagistics).setHasSubtypes(true).setUnlocalizedName(itemName[2]),
-		new ItemDawnstone().setCreativeTab(Magistics.tabMagistics).setUnlocalizedName(itemName[3]),
-		new ItemAmuletDismay().setCreativeTab(Magistics.tabMagistics).setMaxDamage(50).setUnlocalizedName(baubleName[0]),
-		new ItemAmuletLife().setCreativeTab(Magistics.tabMagistics).setMaxDamage(60).setUnlocalizedName(baubleName[1]),
-		new ItemBeltCleansing().setCreativeTab(Magistics.tabMagistics).setMaxDamage(50).setUnlocalizedName(baubleName[2]),
-		new ItemBeltVigor().setCreativeTab(Magistics.tabMagistics).setMaxDamage(100).setUnlocalizedName(baubleName[3])
+		new ItemResources().setHasSubtypes(true).setUnlocalizedName(itemName[0]),
+		new ItemCruelMask(ThaumcraftApi.armorMatThaumium, 2, 0).setMaxDamage(100).setMaxStackSize(1).setUnlocalizedName(itemName[1]),
+		new ItemEldritchKeystone().setHasSubtypes(true).setUnlocalizedName(itemName[2]),
+		new ItemDawnstone().setUnlocalizedName(itemName[3]),
+		new ItemAmuletDismay().setMaxDamage(50).setUnlocalizedName(baubleName[0]),
+		new ItemAmuletLife().setMaxDamage(60).setUnlocalizedName(baubleName[1]),
+		new ItemBeltCleansing().setMaxDamage(50).setUnlocalizedName(baubleName[2]),
+		new ItemBeltVigor().setMaxDamage(100).setUnlocalizedName(baubleName[3])
 	};
 
 	public static Block blocks[] = {
-		new BlockChestHungryEnder().setBlockName(blockName[0]).setCreativeTab(Magistics.tabMagistics).setHardness(22.5F).setLightLevel(0.5F).setResistance(1000F).setStepSound(Block.soundTypePiston),
-		new BlockChestHungryMetal().setBlockName(blockName[1]).setCreativeTab(Magistics.tabMagistics).setHardness(3.0F),
-		//new BlockInfusionWorkbench().setBlockName(blockName[2]).setCreativeTab(Magistics.tabMagistics).setStepSound(Block.soundTypeStone)
+		new BlockChestHungryEnder().setBlockName(blockName[0]).setHardness(22.5F).setLightLevel(0.5F).setResistance(1000F).setStepSound(Block.soundTypePiston),
+		new BlockChestHungryMetal().setBlockName(blockName[1]).setHardness(3.0F)
 	};
 
 	public static Class tiles[] = {
 		TileChestHungryEnder.class,
-		TileChestHungryMetal.class,
-		//TileInfusionWorkbench.class
+		TileChestHungryMetal.class
 	};
 
 	public static void preInit(File configFile) {
@@ -70,7 +68,7 @@ public class MagisticsConfig {
 			debug = config.getBoolean("debug", config.CATEGORY_GENERAL, true, "Toggles the log output of the mod. Great for development, though some people may like logs more silent than others.");
 			low_gfx = config.getBoolean("low_gfx", config.CATEGORY_GENERAL, false, "Toggles or lessens graphical effects created by some blocks & items; great for low-end computers.");
 		} catch (Exception e) {
-			MagisticsLogger.error("An error has occurred while loading configuration properties!", e);
+			error("An error has occurred while loading configuration properties!", e);
 		} finally {
 			if (config.hasChanged())
 				config.save();
@@ -79,11 +77,10 @@ public class MagisticsConfig {
 
 	public static void init() {
 		for (Item item : items)
-			GameRegistry.registerItem(item, item.getUnlocalizedName());
-		for (Class tileEntity : tiles)
-			GameRegistry.registerTileEntity(tileEntity, tileEntity.getSimpleName());
+			GameRegistry.registerItem(item.setCreativeTab(Magistics.tabMagistics), item.getUnlocalizedName());
+		for (Class tile : tiles)
+			GameRegistry.registerTileEntity(tile, tile.getSimpleName());
 		GameRegistry.registerBlock(blocks[0], blockName[0]);
-		//GameRegistry.registerBlock(blocks[2], BlockInfusionWorkbenchItem.class, blockName[2]);
 		ModHandler.init();
 	}
 }
--- src/main/java/T145/magistics/common/config/external/ModHandler.java
+++ src/main/java/T145/magistics/common/config/external/ModHandler.java
@@ -1,94 +1,33 @@
 package T145.magistics.common.config.external;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import thaumcraft.api.ThaumcraftApi;
-import thaumcraft.api.aspects.Aspect;
-import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.research.ResearchCategories;
-import thaumcraft.api.research.ResearchItem;
-import thaumcraft.api.research.ResearchPage;
-import thaumcraft.common.config.ConfigBlocks;
-import thaumcraft.common.config.ConfigResearch;
 import T145.magistics.common.Magistics;
 import T145.magistics.common.blocks.BlockChestHungryMetalItem;
 import T145.magistics.common.config.MagisticsConfig;
-import T145.magistics.common.config.MagisticsLogger;
-import T145.magistics.common.lib.ResearchPages;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.ironchest.IronChest;
 
-public class ModHandler {
+public class ModHandler extends MagisticsConfig {
 	public static void init() {
-		if (Loader.isModLoaded("BuildCraft|Transport")) {
-			MagisticsLogger.log("Buildcraft detected; compatibility loaded.");
-			//addFacades(MagisticsConfig.blocks[2].getUnlocalizedName(), 0, 1);
-		}
 		if (Loader.isModLoaded("IronChest")) {
-			MagisticsLogger.log("IronChest detected; support loaded.");
-			GameRegistry.registerBlock(MagisticsConfig.blocks[1], BlockChestHungryMetalItem.class, MagisticsConfig.blockName[1]);
-
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 0), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.METAL, 4));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 1), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.METAL, 4).add(Aspect.GREED, 4));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 2), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.GREED, 2).add(Aspect.CRYSTAL, 4));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 3), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.METAL, 4).add(Aspect.EXCHANGE, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 4), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.METAL, 4).add(Aspect.GREED, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 5), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.GREED, 4).add(Aspect.CRYSTAL, 4).add(Aspect.ORDER, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 6), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.EARTH, 4).add(Aspect.FIRE, 4).add(Aspect.DARKNESS, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(IronChest.ironChestBlock, 1, 7), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.EARTH, 6));
-
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 0), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.METAL, 4));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 1), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.METAL, 4).add(Aspect.GREED, 4));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 2), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.GREED, 2).add(Aspect.CRYSTAL, 4));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 3), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.METAL, 4).add(Aspect.EXCHANGE, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 4), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.METAL, 4).add(Aspect.GREED, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 5), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.GREED, 4).add(Aspect.CRYSTAL, 4).add(Aspect.ORDER, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 6), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.EARTH, 4).add(Aspect.FIRE, 4).add(Aspect.DARKNESS, 2));
-			ThaumcraftApi.registerObjectTag(new ItemStack(MagisticsConfig.blocks[1], 1, 7), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 3).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.EARTH, 6));
-
-			ConfigResearch.recipes.put("HungryCopperChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 3), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "WWW", "WTW", "WWW", 'W', "ingotCopper", 'T', ConfigBlocks.blockChestHungry));
-			ConfigResearch.recipes.put("HungryIronChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 0), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "WWW", "WTW", "WWW", 'W', "ingotIron", 'T', ConfigBlocks.blockChestHungry));
-			ConfigResearch.recipes.put("HungryIronChestAlt", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 0), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "ABA", "BCB", "ABA", 'A', "ingotIron", 'B', "blockGlass", 'C', new ItemStack(MagisticsConfig.blocks[1], 1, 3)));
-			ConfigResearch.recipes.put("HungrySilverChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 4), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "ABA", "AAA", 'A', "ingotSilver", 'B', new ItemStack(MagisticsConfig.blocks[1], 1, 3)));
-			ConfigResearch.recipes.put("HungrySilverChestAlt", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 4), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "ABA", "BCB", "ABA", 'A', "ingotSilver", 'B', "blockGlass", 'C', new ItemStack(MagisticsConfig.blocks[1], 1, 0)));
-			ConfigResearch.recipes.put("HungryGoldChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 1), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "ABA", "AAA", 'A', "ingotGold", 'B', new ItemStack(MagisticsConfig.blocks[1], 1, 0)));
-			ConfigResearch.recipes.put("HungryGoldChestAlt", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 1), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "ABA", "BCB", "ABA", 'A', "ingotGold", 'B', "blockGlass", 'C', new ItemStack(MagisticsConfig.blocks[1], 1, 4)));
-			ConfigResearch.recipes.put("HungryDiamondChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 2), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "ABA", "CCC", 'A', "blockGlass", 'B', new ItemStack(MagisticsConfig.blocks[1], 1, 4), 'C', "gemDiamond"));
-			ConfigResearch.recipes.put("HungryDiamondChestAlt", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 2), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "BCB", "AAA", 'A', "blockGlass", 'B', "gemDiamond", 'C', new ItemStack(MagisticsConfig.blocks[1], 1, 1)));
-			ConfigResearch.recipes.put("HungryCrystalChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 5), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "ABA", "AAA", 'A', "blockGlass", 'B', new ItemStack(MagisticsConfig.blocks[1], 1, 2)));
-			ConfigResearch.recipes.put("HungryObsidianChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 6), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "ABA", "AAA", 'A', "blockObsidian", 'B', new ItemStack(MagisticsConfig.blocks[1], 1, 5)));
-			ConfigResearch.recipes.put("HungryDirtChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTIRON", new ItemStack(MagisticsConfig.blocks[1], 1, 5), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "AAA", "ABA", "AAA", 'A', "blockDirt", 'B', ConfigBlocks.blockChestHungry));
+			log("IronChest detected; support loaded.");
+			GameRegistry.registerBlock(blocks[1], BlockChestHungryMetalItem.class, blockName[1]);
 		}
 	}
 
-	public static void postInit() {
+	public static void postInit() { // TODO: Add more Thaumcraft stuff
 		ResearchCategories.registerCategory(Magistics.modid.toUpperCase(), new ResourceLocation("magistics", "textures/gui/tab.png"), new ResourceLocation("thaumcraft", "textures/gui/gui_researchback.png"));
-
-		ThaumcraftApi.registerObjectTag(new ItemStack(ConfigBlocks.blockChestHungry), new AspectList().add(Aspect.VOID, 2).add(Aspect.MOTION, 2).add(Aspect.AIR, 2).add(Aspect.HUNGER, 2).add(Aspect.TREE, 6));
-		ThaumcraftApi.registerObjectTag(new ItemStack(ConfigBlocks.blockArcaneFurnace), new AspectList().add(Aspect.FIRE, 8).add(Aspect.CRAFT, 4).add(Aspect.AURA, 4).add(Aspect.ENTROPY, 2));
-
-		ThaumcraftApi.registerComplexObjectTag(new ItemStack(MagisticsConfig.blocks[0], 1, 32767), new AspectList().merge(Aspect.EXCHANGE, 2).merge(Aspect.TRAVEL, 2).merge(Aspect.VOID, 4));
-
-		ConfigResearch.recipes.put("HungryEnderChest", ThaumcraftApi.addArcaneCraftingRecipe("HUNGRYCHESTENDER", new ItemStack(MagisticsConfig.blocks[0]), new AspectList().add(Aspect.AIR, 5).add(Aspect.ORDER, 3).add(Aspect.ENTROPY, 3), "ABA", "ACA", "AAA", 'A', Blocks.obsidian, 'B', new ItemStack(ConfigBlocks.blockMetalDevice, 1, 5), 'C', Items.ender_eye));
-
-		new ResearchItem("HUNGRYCHESTENDER", Magistics.modid.toUpperCase(), new AspectList().add(Aspect.HUNGER, 3).add(Aspect.VOID, 3).add(Aspect.EXCHANGE, 2).add(Aspect.TRAVEL, 2).add(Aspect.VOID, 4), -1, 0, 2, new ItemStack(MagisticsConfig.blocks[0])).setPages(new ResearchPage("tc.research_page.HUNGRYCHESTENDER.1"), new ResearchPages().arcaneRecipe("HungryEnderChest")).setSecondary().setParents("HUNGRYCHEST").registerResearchItem();
-		ThaumcraftApi.addWarpToResearch("HUNGRYCHESTENDER", 1);
-
-		if (Loader.isModLoaded("IronChest"))
-			new ResearchItem("HUNGRYCHESTIRON", Magistics.modid.toUpperCase(), new AspectList().add(Aspect.HUNGER, 3).add(Aspect.VOID, 3), 1, 1, 1, new ItemStack(MagisticsConfig.blocks[1], 1, 32767)).setPages(new ResearchPage("tc.research_page.HUNGRYCHESTIRON.1"), new ResearchPages().arcaneRecipe("HungryIronChest"), new ResearchPages().arcaneRecipe("HungryIronChestAlt"), new ResearchPages().arcaneRecipe("HungryGoldChest"), new ResearchPages().arcaneRecipe("HungryGoldChestAlt"), new ResearchPages().arcaneRecipe("HungryDiamondChest"), new ResearchPages().arcaneRecipe("HungryDiamondChestAlt"), new ResearchPages().arcaneRecipe("HungryCopperChest"), new ResearchPages().arcaneRecipe("HungrySilverChest"), new ResearchPages().arcaneRecipe("HungrySilverChestAlt"), new ResearchPages().arcaneRecipe("HungryCrystalChest"), new ResearchPages().arcaneRecipe("HungryObsidianChest"), new ResearchPages().arcaneRecipe("HungryDirtChest")).setSecondary().registerResearchItem();
 	}
 
-	public static void addFacades(String blockName, int meta_start, int meta_end) {
-		for (int meta = meta_start; meta <= meta_end; meta++)
+	public static void addFacades(String blockName, int metaStart, int metaEnd) {
+		for (int meta = metaStart; meta <= metaEnd; meta++)
 			addFacade(blockName, meta);
 	}
 
 	public static void addFacade(String blockName, int meta) {
-		MagisticsLogger.log("Adding facades for " + blockName + "." + meta);
+		log("Adding facades for " + blockName + "." + meta);
 		FMLInterModComms.sendMessage("BuildCraft|Transport", "add-facade", blockName + "@" + meta);
 	}
 }
