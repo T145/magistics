6ae77a27539b0227e42982b47273596da8431632
T145 <taylorshuler@aol.com>
2016-05-07 18:24:54 -0400

    Working from scratch

--- src/main/java/T145/magistics/common/tiles/TileCrystalizer.java
+++
@@ -1,9 +0,0 @@
-package T145.magistics.common.tiles;
-
-import thaumcraft.api.TileThaumcraft;
-
-public class TileCrystalizer extends TileThaumcraft {
-	public boolean isCooking() {
-		return false;
-	}
-}
