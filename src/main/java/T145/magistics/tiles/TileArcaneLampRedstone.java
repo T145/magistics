98f95bd9c3b71e17ccce41a9273ae52a26a9217c
T145 <taylorshuler@aol.com>
2016-05-26 10:44:09 -0400

    Be sure the Arcane Redstone Lamp doesn't conflict w/ other Arcane Lamps

--- src/main/java/T145/magistics/tiles/TileArcaneLampRedstone.java
+++ src/main/java/T145/magistics/tiles/TileArcaneLampRedstone.java
@@ -1,15 +1,73 @@
 package T145.magistics.tiles;
 
+import net.minecraft.block.Block;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import thaumcraft.common.config.ConfigBlocks;
 import thaumcraft.common.tiles.TileArcaneLamp;
 import T145.magistics.blocks.BlockArcaneLampRedstone;
 
 public class TileArcaneLampRedstone extends TileArcaneLamp {
+	private boolean conflictingLamp = false;
+
+	public boolean hasNeighborLamp() {
+		return conflictingLamp;
+	}
+
+	public boolean isActive() {
+		return getBlockType() == BlockArcaneLampRedstone.ACTIVE;
+	}
+
 	@Override
 	public void updateEntity() {
-		if (getBlockType() == BlockArcaneLampRedstone.ACTIVE) {
+		if (isActive()) {
 			super.updateEntity();
+			conflictingLamp = false;
 		} else {
 			removeLights();
+		}
+	}
+
+	@Override
+	public void readCustomNBT(NBTTagCompound tag) {
+		conflictingLamp = tag.getBoolean("neighboringLamp");
+	}
+
+	@Override
+	public void writeCustomNBT(NBTTagCompound tag) {
+		tag.setBoolean("neighborLamp", conflictingLamp);
+	}
+
+	@Override
+	public void removeLights() {
+		for (int x = -15; x <= 15; x++) {
+			for (int y = -15; y <= 15; y++) {
+				for (int z = -15; z <= 15; z++) {
+					int xx = xCoord + x;
+					int yy = yCoord + y;
+					int zz = zCoord + z;
+					Block dest = worldObj.getBlock(xx, yy, zz);
+					TileEntity tile = worldObj.getTileEntity(xx, yy, zz);
+
+					if (tile != null && tile instanceof TileArcaneLamp) {
+						if (tile instanceof TileArcaneLampRedstone) {
+							TileArcaneLampRedstone lamp = (TileArcaneLampRedstone) tile;
+
+							if (lamp.isActive()) {
+								conflictingLamp = true;
+							}
+						} else {
+							conflictingLamp = true;
+						}
+					}
+
+					if (!conflictingLamp) {
+						if (worldObj.getBlock(xCoord + x, yCoord + y, zCoord + z) == ConfigBlocks.blockAiry && worldObj.getBlockMetadata(xCoord + x, yCoord + y, zCoord + z) == 3) {
+							worldObj.setBlockToAir(xCoord + x, yCoord + y, zCoord + z);
+						}
+					}
+				}
+			}
 		}
 	}
 }
