7bb8ca4b0be9f7c0b52003f0ee8de9d3651293aa
T145 <taylorshuler@aol.com>
2016-06-25 14:28:17 -0400

    Added particle fx to the crystal core

--- src/main/java/T145/magistics/tiles/TileCrystalCore.java
+++ src/main/java/T145/magistics/tiles/TileCrystalCore.java
@@ -17,7 +17,9 @@ import thaumcraft.api.aspects.AspectList;
 import thaumcraft.api.nodes.INode;
 import thaumcraft.api.nodes.NodeType;
 import thaumcraft.api.wands.IWandable;
+import thaumcraft.common.Thaumcraft;
 import thaumcraft.common.blocks.BlockCosmeticSolid;
+import thaumcraft.common.config.Config;
 import thaumcraft.common.lib.world.ThaumcraftWorldGenerator;
 import thaumcraft.common.tiles.TileJarNode;
 
@@ -27,7 +29,6 @@ public class TileCrystalCore extends TileThaumcraft implements IWandable {
 
 	private boolean active = false;
 	private boolean draining = false;
-	private boolean hasCore = false;
 	private boolean neighborChange = false;
 
 	private int count = -1;
@@ -35,6 +36,7 @@ public class TileCrystalCore extends TileThaumcraft implements IWandable {
 	public int drainX = 0;
 	public int drainY = 0;
 	public int drainZ = 0;
+	public int drainColor = 0;
 	private List<ChunkCoordinates> nodes = null;
 
 	public boolean isActive() {
@@ -63,6 +65,11 @@ public class TileCrystalCore extends TileThaumcraft implements IWandable {
 				&& isObsidianTotem(xCoord + 1, yCoord - 2, zCoord + 1)
 				&& isObsidianTotem(xCoord - 1, yCoord - 2, zCoord + 1)
 				&& isObsidianTotem(xCoord + 1, yCoord - 2, zCoord - 1);
+	}
+
+	public boolean canActivate() {
+		Block block = worldObj.getBlock(xCoord, yCoord + 1, zCoord);
+		return block.getMaterial() == Material.air || block.getMaterial() == Config.airyMaterial && hasWorldObj() && !active && hasAltar() && hasNodes();
 	}
 
 	@Override
@@ -106,62 +113,90 @@ public class TileCrystalCore extends TileThaumcraft implements IWandable {
 				speed = 0F;
 			}
 
-			boolean discoverNodes = false;
-
-			if (count % 20 == 0 && neighborChange && hasNodes()) {
-				worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, getBlockType());
-				neighborChange = false;
-			}
-
-			if (active && speed > 0.9F && count % 20 == 0) {
-				draining = false;
-
-				if (!hasCore) {
-					hasCore = true;
-					ThaumcraftWorldGenerator.createNodeAt(worldObj, xCoord, yCoord + 1, zCoord, NodeType.NORMAL, null, new AspectList());
-				}
-
-				INode core = (INode) worldObj.getTileEntity(xCoord, yCoord + 1, zCoord);
-
-				for (ChunkCoordinates coord : nodes) {
-					int x = coord.posX;
-					int y = coord.posY;
-					int z = coord.posZ;
-
-					TileEntity tile = worldObj.getTileEntity(x, y, z);
-
-					if (tile != null && tile instanceof INode && !(tile instanceof TileJarNode) && tile != core) {
-						INode node = (INode) tile;
-
-						for (Aspect aspect : node.getAspects().getAspects()) {
-							if (node.getAspects().getAmount(aspect) > 0) {
-								node.takeFromContainer(aspect, 1);
-								core.getAspects().add(aspect, 1);
-
-								if (!worldObj.isRemote && !draining) {
-									discoverNodes = true;
-								}
-
-								neighborChange = true;
-								draining = true;
-								drainX = x;
-								drainY = y;
-								drainZ = z;
-
-								if (!draining) {
-									discoverNodes = true;
-								}
-								return;
-							}
-						}
-
-						if (node.getAspects().visSize() == 0) {
-							worldObj.setBlockToAir(x, y, z);
-						}
+			if (worldObj.isRemote && active && speed > 0.9F) {
+				if (count % 2 == 0) {
+					switch (count % 8 / 2) {
+					case 0:
+						Thaumcraft.proxy.blockRunes(worldObj, xCoord - 1, yCoord - 1, zCoord - 1, 0.3F + worldObj.rand.nextFloat() * 0.7F, 0F, 0.3F + worldObj.rand.nextFloat() * 0.7F, 20, 0.03F);
+						break;
+					case 1:
+						Thaumcraft.proxy.blockRunes(worldObj, xCoord + 1, yCoord - 1, zCoord + 1, 0.3F + worldObj.rand.nextFloat() * 0.7F, 0F, 0.3F + worldObj.rand.nextFloat() * 0.7F, 20, 0.03F);
+						break;
+					case 2:
+						Thaumcraft.proxy.blockRunes(worldObj, xCoord - 1, yCoord - 1, zCoord + 1, 0.3F + worldObj.rand.nextFloat() * 0.7F, 0F, 0.3F + worldObj.rand.nextFloat() * 0.7F, 20, 0.03F);
+						break;
+					case 3:
+						Thaumcraft.proxy.blockRunes(worldObj, xCoord + 1, yCoord - 1, zCoord - 1, 0.3F + worldObj.rand.nextFloat() * 0.7F, 0F, 0.3F + worldObj.rand.nextFloat() * 0.7F, 20, 0.03F);
+						break;
 					}
 				}
 
-				deactivate();
+				if (count % 20 == 0 && draining) {
+					Thaumcraft.proxy.beam(worldObj, xCoord + 0.5, yCoord + 0.5 + speed, zCoord + 0.5, drainX + 0.5, drainY + 0.5, drainZ + 0.5, 1, drainColor, true, 1.5F, 20);
+				}
+			}
+
+			boolean discoverNodes = false;
+
+			if (count % 20 == 0) {
+				if (neighborChange && hasNodes()) {
+					worldObj.notifyBlocksOfNeighborChange(xCoord, yCoord, zCoord, getBlockType());
+					neighborChange = false;
+				}
+
+				if (active && speed > 0.9F) {
+					if (!(worldObj.getTileEntity(xCoord, yCoord + 1, zCoord) instanceof INode)) {
+						ThaumcraftWorldGenerator.createNodeAt(worldObj, xCoord, yCoord + 1, zCoord, NodeType.NORMAL, null, new AspectList());
+					}
+
+					INode core = (INode) worldObj.getTileEntity(xCoord, yCoord + 1, zCoord);
+
+					if (hasAltar() && core != null) {
+						for (ChunkCoordinates coord : nodes) {
+							int x = coord.posX;
+							int y = coord.posY;
+							int z = coord.posZ;
+
+							TileEntity tile = worldObj.getTileEntity(x, y, z);
+
+							if (tile != null && tile != core) {
+								INode node = (INode) tile;
+
+								for (Aspect aspect : node.getAspects().getAspects()) {
+									if (node.getAspects().getAmount(aspect) > 0) {
+										node.takeFromContainer(aspect, 1);
+										core.getAspects().add(aspect, 1);
+
+										if (!worldObj.isRemote && !draining) {
+											discoverNodes = true;
+										}
+
+										neighborChange = true;
+										draining = true;
+										drainColor = aspect.getColor();
+										drainX = x;
+										drainY = y;
+										drainZ = z;
+
+										if (!draining) {
+											discoverNodes = true;
+										}
+										return;
+									}
+								}
+
+								if (node.getAspects().visSize() == 0) {
+									draining = false;
+									worldObj.setBlockToAir(x, y, z);
+									Thaumcraft.proxy.burst(worldObj, x + 0.5D, y + 0.5D, z + 0.5D, 1F);
+									worldObj.playSound(x + 0.5D, y + 0.5D, z + 0.5D, "thaumcraft:craftfail", 1F, 1F, false);
+								}
+							}
+						}
+					}
+
+					deactivate();
+				}
 			}
 
 			if (count % 100 == 0 && (discoverNodes || nodes.size() == 0)) {
@@ -188,7 +223,7 @@ public class TileCrystalCore extends TileThaumcraft implements IWandable {
 					int zz = zCoord + z;
 					TileEntity tile = worldObj.getTileEntity(xx, yy, zz);
 
-					if (tile != null && tile instanceof INode && !(tile instanceof TileJarNode)) {
+					if (tile != null && tile instanceof INode && !(tile instanceof TileJarNode) && tile != worldObj.getTileEntity(xCoord, yCoord + 1, zCoord)) {
 						nodes.add(new ChunkCoordinates(xx, yy, zz));
 					}
 				}
@@ -198,7 +233,7 @@ public class TileCrystalCore extends TileThaumcraft implements IWandable {
 
 	@Override
 	public int onWandRightClick(World world, ItemStack wandstack, EntityPlayer player, int x, int y, int z, int side, int md) {
-		if (!world.isRemote && !active && hasAltar() && worldObj.getBlock(xCoord, yCoord, zCoord).getMaterial() == Material.air) {
+		if (canActivate()) {
 			active = true;
 			worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
 			markDirty();
