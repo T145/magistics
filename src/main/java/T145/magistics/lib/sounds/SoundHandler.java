e104021ea7197482b728a080454a8922c97eef5b
Taylor Shuler <taylorshuler@aol.com>
2016-10-20 11:07:31 -0400

    Fixed sound handling

--- src/main/java/T145/magistics/lib/sounds/SoundHandler.java
+++ src/main/java/T145/magistics/lib/sounds/SoundHandler.java
@@ -1,13 +1,29 @@
 package T145.magistics.lib.sounds;
 
 import T145.magistics.Magistics;
+import net.minecraft.client.audio.SoundRegistry;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
 
 public class SoundHandler {
 
-	public static CustomSoundEvent BUBBLING;
+	public static SoundEvent bubbling;
+
+	private static int size = 0;
 
 	public static void registerSounds() {
-		BUBBLING = new CustomSoundEvent(new ResourceLocation(Magistics.MODID, "bubbling"));
+		size = SoundEvent.REGISTRY.getKeys().size();
+		bubbling = registerSound("bubbling");
+	}
+
+	private static SoundEvent registerSound(String name) {
+		ResourceLocation loc = new ResourceLocation(Magistics.MODID, name);
+		SoundEvent sound = new SoundEvent(loc);
+
+		SoundEvent.REGISTRY.register(size, loc, sound);
+		++size;
+
+		return sound;
 	}
 }
