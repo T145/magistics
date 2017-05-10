f22be68a90ca6d91d2292f09b7f7d02038a2283f
T145 <taylorshuler@aol.com>
2017-05-10 01:20:06 -0400

    Heavily refined Elevator logic
    TODO: Fix sounds? This one was the only one that played too :’(

---
+++ src/main/java/T145/magistics/api/variants/items/ResearchType.java
@@ -0,0 +1,6 @@
+package T145.magistics.api.variants.items;
+
+public enum ResearchType {
+
+	LOST, FORBIDDEN, BLIGHTED, ELDRITCH;
+}
--- src/main/java/T145/magistics/tiles/devices/TileElevator.java
+++ src/main/java/T145/magistics/tiles/devices/TileElevator.java
@@ -1,99 +1,102 @@
 package T145.magistics.tiles.devices;
 
-import java.util.List;
+import java.util.Stack;
 
 import T145.magistics.tiles.MTile;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 
 public class TileElevator extends MTile {
 
-	private boolean blocked;
-	private int range = 14;
+	private final int RANGE = 14;
+	private int cooldown;
 	private int delay;
+	private boolean clear;
 
-	public boolean isBlocked() {
-		return blocked;
+	public int getCooldown() {
+		return cooldown;
+	}
+
+	public void setCooldown(int cooldown) {
+		this.cooldown = cooldown;
+	}
+
+	public boolean isClear() {
+		return clear;
 	}
 
 	public boolean isPowered() {
 		return world.isBlockPowered(pos);
 	}
 
-	@Override
-	public void writePacketNBT(NBTTagCompound compound) {}
-
-	@Override
-	public void readPacketNBT(NBTTagCompound compound) {}
-
-	@Override
-	public void update() {
+	public Stack<Entity> getEntitiesAbove() {
 		double xx = pos.getX() + 0.5D;
 		double yy = pos.getY() + 1D;
 		double zz = pos.getZ() + 0.5D;
+		AxisAlignedBB boxAbove = new AxisAlignedBB(xx, yy, zz, xx, yy + 1D, zz);
+		Stack<Entity> entitiesAbove = new Stack<Entity>();
+		entitiesAbove.addAll(world.getEntitiesWithinAABB(Entity.class, boxAbove));
+		return entitiesAbove;
+	}
 
-		List<EntityPlayer> targets = world.getEntitiesWithinAABB(EntityPlayer.class, new AxisAlignedBB(xx, yy, zz, xx, yy + 1D, zz));
+	@Override
+	public void writePacketNBT(NBTTagCompound compound) {
+		compound.setInteger("Cooldown", cooldown);
+	}
 
-		if (targets.size() == 1) {
-			EntityPlayer player = targets.get(0);
-			double destY = 0;
-			double endY = 0;
+	@Override
+	public void readPacketNBT(NBTTagCompound compound) {
+		cooldown = compound.getInteger("Cooldown");
+	}
 
-			++delay;
-			blocked = true;
+	@Override
+	public void update() { // "signal strength" can also be weakened by adding consecutive cooldowns
+		if ((cooldown = Math.max(cooldown - 1, 0)) == 0) {
+			Stack<Entity> entitiesAbove = getEntitiesAbove();
 
-			if (isPowered()) {
-				destY = yy - 1D;
-				endY = range - destY;
+			if (!(clear = entitiesAbove.isEmpty())) {
+				Entity target = entitiesAbove.pop();
 
-				if (endY < 0) {
-					endY = 0;
-				}
-			} else {
-				destY = yy + 1D;
-				endY = range + destY;
+				if (++delay % 30 == 0) {
+					delay = 0;
 
-				if (endY > world.getActualHeight()) {
-					endY = world.getActualHeight();
+					for (int offsetY = 1; offsetY <= RANGE; ++offsetY) {
+						BlockPos destPos = pos.offset(isPowered() ? EnumFacing.DOWN : EnumFacing.UP, offsetY);
+
+						if (destPos.getY() < 0) {
+							destPos = new BlockPos(pos.getX(), 0, pos.getZ());
+						} else if (destPos.getY() > world.getActualHeight()) {
+							destPos = pos;
+						}
+
+						TileEntity tile = world.getTileEntity(destPos);
+
+						if (tile instanceof TileElevator && tile != this) {
+							TileElevator dest = (TileElevator) tile;
+
+							if (dest.canTeleportTo()) {
+								target.setPositionAndUpdate(target.posX, destPos.getY() + 1D, target.posZ);
+								world.playSound(null, pos.getX(), pos.getY(), pos.getX(), SoundEvents.ENTITY_ENDERMEN_TELEPORT, SoundCategory.HOSTILE, 1F, 1F);
+								dest.setCooldown(20);
+								break;
+							}
+						}
+					}
 				}
 			}
-
-			if (delay % 45 == 0) {
-				delay = 0;
-
-				while (destY != endY) {
-					if (isPowered()) {
-						--destY;
-					} else {
-						++destY;
-					}
-
-					BlockPos destPos = new BlockPos(pos.getX(), destY, pos.getZ());
-					TileEntity tile = world.getTileEntity(destPos);
-					TileElevator elevator = (TileElevator) tile;
-
-					if (elevator != null && !elevator.isBlocked() && !elevator.isPowered() && canTeleportTo(destPos)) {
-						player.setPositionAndUpdate(player.posX, destY + 1D, player.posZ);
-						world.playSound(null, pos.getX(), pos.getY(), pos.getX(), SoundEvents.ENTITY_ENDERMEN_TELEPORT, SoundCategory.HOSTILE, 1F, 1F);
-						return;
-					}
-				}
-			}
-		} else {
-			blocked = false;
-			return;
 		}
 	}
 
-	private boolean canTeleportTo(BlockPos pos) {
-		return canTeleportAt(pos.up()) && canTeleportAt(pos.up(2));
+	public boolean canTeleportTo() {
+		return clear && !isPowered() && canTeleportAt(pos.up()) && canTeleportAt(pos.up(2));
 	}
 
 	private boolean canTeleportAt(BlockPos pos) {
