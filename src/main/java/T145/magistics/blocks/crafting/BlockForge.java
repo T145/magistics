50652e17d9dd5c6accc23e7d6222c1b429a1bc3d
T145 <taylorshuler@aol.com>
2017-05-21 23:08:36 -0400

    Forge block base

--- src/main/java/T145/magistics/blocks/crafting/BlockForge.java
+++ src/main/java/T145/magistics/blocks/crafting/BlockForge.java
@@ -1,12 +1,28 @@
 package T145.magistics.blocks.crafting;
 
+import java.util.Random;
+
+import T145.magistics.Magistics;
 import T145.magistics.api.logic.IHorizontalFacing;
 import T145.magistics.api.variants.blocks.EnumForge;
 import T145.magistics.blocks.MBlockDevice;
+import T145.magistics.init.ModBlocks;
 import T145.magistics.tiles.crafting.TileForge;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.Container;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockForge extends MBlockDevice<EnumForge> implements IHorizontalFacing {
 
@@ -17,5 +33,78 @@ public class BlockForge extends MBlockDevice<EnumForge> implements IHorizontalFa
 	@Override
 	public TileEntity createNewTileEntity(World world, int meta) {
 		return new TileForge();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	@SuppressWarnings("incomplete-switch")
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		if (state.getValue(WORKING)) {
+			double d0 = pos.getX() + 0.5D;
+			double d1 = pos.getY() + rand.nextDouble() * 6.0D / 16.0D;
+			double d2 = pos.getZ() + 0.5D;
+			double d3 = 0.52D;
+			double d4 = rand.nextDouble() * 0.6D - 0.3D;
+
+			if (rand.nextDouble() < 0.1D) {
+				world.playSound(pos.getX() + 0.5D, pos.getY(), pos.getZ() + 0.5D, SoundEvents.BLOCK_FURNACE_FIRE_CRACKLE, SoundCategory.BLOCKS, 1.0F, 1.0F, false);
+			}
+
+			switch (state.getValue(FACING)) {
+			case WEST:
+				world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0 - 0.52D, d1, d2 + d4, 0.0D, 0.0D, 0.0D, new int[0]);
+				world.spawnParticle(EnumParticleTypes.FLAME, d0 - 0.52D, d1, d2 + d4, 0.0D, 0.0D, 0.0D, new int[0]);
+				break;
+			case EAST:
+				world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0 + 0.52D, d1, d2 + d4, 0.0D, 0.0D, 0.0D, new int[0]);
+				world.spawnParticle(EnumParticleTypes.FLAME, d0 + 0.52D, d1, d2 + d4, 0.0D, 0.0D, 0.0D, new int[0]);
+				break;
+			case NORTH:
+				world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0 + d4, d1, d2 - 0.52D, 0.0D, 0.0D, 0.0D, new int[0]);
+				world.spawnParticle(EnumParticleTypes.FLAME, d0 + d4, d1, d2 - 0.52D, 0.0D, 0.0D, 0.0D, new int[0]);
+				break;
+			case SOUTH:
+				world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, d0 + d4, d1, d2 + 0.52D, 0.0D, 0.0D, 0.0D, new int[0]);
+				world.spawnParticle(EnumParticleTypes.FLAME, d0 + d4, d1, d2 + 0.52D, 0.0D, 0.0D, 0.0D, new int[0]);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+		if (!world.isRemote && hand.equals(EnumHand.MAIN_HAND) && world.getTileEntity(pos) instanceof TileForge) {
+			player.openGui(Magistics.MODID, 1, world, pos.getX(), pos.getY(), pos.getZ());
+		}
+
+		return true;
+	}
+
+	public static void setState(boolean active, World world, BlockPos pos) {
+		IBlockState state = world.getBlockState(pos);
+		TileEntity tile = world.getTileEntity(pos);
+
+		world.setBlockState(pos, ModBlocks.forge.getDefaultState().withProperty(WORKING, active).withProperty(FACING, state.getValue(FACING)), 3);
+		world.setBlockState(pos, ModBlocks.forge.getDefaultState().withProperty(WORKING, active).withProperty(FACING, state.getValue(FACING)), 3);
+
+		if (tile != null) {
+			tile.validate();
+			world.setTileEntity(pos, tile);
+		}
+	}
+
+	@Override
+	public int getLightValue(IBlockState state, IBlockAccess world, BlockPos pos) {
+		return state.getValue(WORKING) ? 13 : 0;
+	}
+
+	@Override
+	public boolean hasComparatorInputOverride(IBlockState state) {
+		return true;
+	}
+
+	@Override
+	public int getComparatorInputOverride(IBlockState state, World world, BlockPos pos) {
+		return Container.calcRedstone(world.getTileEntity(pos));
 	}
 }
