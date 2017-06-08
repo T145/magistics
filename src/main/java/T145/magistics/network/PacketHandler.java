19b4bee270304c710ef9680f3886355e7d74476e
T145 <taylorshuler@aol.com>
2017-06-07 22:33:09 -0400

    Refined network message registration

--- src/main/java/T145/magistics/network/PacketHandler.java
+++ src/main/java/T145/magistics/network/PacketHandler.java
@@ -3,14 +3,9 @@ package T145.magistics.network;
 import T145.magistics.Magistics;
 import T145.magistics.network.messages.MessageBase;
 import T145.magistics.network.messages.client.MessageInfuserProgress;
-import net.minecraft.client.Minecraft;
-import net.minecraft.server.MinecraftServer;
-import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraft.util.IThreadListener;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
-import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -25,49 +20,10 @@ public class PacketHandler {
 	}
 
 	public static void registerMessage(Class<? extends MessageBase> message, Side side) {
-		INSTANCE.registerMessage(getHandler(message, side), message, id++, side);
-	}
-
-	private static IMessageHandler<MessageBase, IMessage> getHandler(Class<? extends MessageBase> messageType, Side side) {
-		switch (side) {
-		case CLIENT:
-			return new ClientMessageHandler();
-		case SERVER:
-			return new CommonMessageHandler();
-		default:
+		INSTANCE.registerMessage((m, ctx) -> {
+			IThreadListener thread = FMLCommonHandler.instance().getWorldThread(ctx.netHandler);
+			thread.addScheduledTask(() -> m.process(ctx));
 			return null;
-		}
-	}
-
-	private static class CommonMessageHandler implements IMessageHandler<MessageBase, IMessage> {
-
-		@Override
-		public IMessage onMessage(MessageBase message, MessageContext context) {
-			MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
-
-			try {
-				server.callFromMainThread(() -> message.process(context));
-			} catch (Exception err) {
-				Magistics.LOGGER.catching(err);
-			}
-
-			return null;
-		}
-	}
-
-	private static class ClientMessageHandler implements IMessageHandler<MessageBase, IMessage> {
-
-		@Override
-		public IMessage onMessage(MessageBase message, MessageContext context) {
-			Minecraft mc = FMLClientHandler.instance().getClient();
-
-			try {
-				mc.addScheduledTask(() -> message.process(context));
-			} catch (Exception err) {
-				Magistics.LOGGER.catching(err);
-			}
-
-			return null;
-		}
+		}, message, id++, side);
 	}
 }
